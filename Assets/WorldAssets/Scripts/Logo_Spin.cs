5e414f7ce6ea8bd9b2ad41e8b3e6ceaa878c19a0
Darcody <d-jordan@gmx.net>
2015-05-14 13:57:37 +0200

    Erstellung Worldassets mit unterordner. Einfügen der Statue mit Modell, Material, Textures und Erstellung des Prefab davon.

---
+++ Assets/WorldAssets/Scripts/Logo_Spin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Logo_Spin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (transform.position, Vector3.down, Time.deltaTime*50.0f);
+	}
+}
