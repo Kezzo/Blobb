ee51d54ed19ccd8bd5bd9159b3153bf7c5903b80
darcody <d-jordan@gmx.net>
2015-06-06 04:40:51 +0200

    Added Level 0

---
+++ Assets/GateOpener.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateOpener : MonoBehaviour {
+	public GameObject doorA;
+	public GameObject doorB;
+	bool open=false;
+	bool close=false;
+	int opentime=0;
+	Vector3 startPosA;
+	Vector3 startPosB;
+
+	public bool testmode;
+
+	// Use this for initialization
+	void Start () {
+	
+		if (doorA) {
+		
+			startPosA=doorA.transform.position;
+			startPosB=doorB.transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (open == true) {
+
+			if(opentime<50)
+			{//print ("Open"+opentime);
+				if (doorA) {
+					doorA.transform.position += new Vector3(-0.1f,0,0);
+					doorB.transform.position += new Vector3(0.1f,0,0);
+					
+				}
+
+				opentime++;
+			}
+
+		
+		}
+
+
+		if (close==true) {
+			
+
+				if (doorA) {
+					//doorA.transform.position += new Vector3(0.1f,0,0);
+					//doorB.transform.position += new Vector3(-0.1f,0,0);
+					doorA.transform.position = Vector3.Lerp(doorA.transform.position, startPosA, Time.deltaTime);
+					doorB.transform.position = Vector3.Lerp(doorB.transform.position, startPosB, Time.deltaTime);
+					
+				}
+				
+				
+
+			
+			
+		}
+	}
+
+	void OnTriggerEnter(Collider enter) {
+		if (enter.name.Contains ("container")) {
+			open = true;
+			close = false;
+		}
+		//print ("You shall pass!");
+		opentime = 0;
+	}
+	void OnTriggerExit(Collider exit) {
+		//print ("You passed!");
+		opentime = 0;
+
+		open = false;
+		close = true;
+
+	}
+}
---
+++ Assets/WorldAssets/glasscontainer/Scripts/CraneMover.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraneMover : MonoBehaviour {
+	public float distance=150;
+	[Range(0.1f,0.6f)]
+	public float speed=0.1f;
+    float timemultiplier=0.01f;
+	float i;
+	public float height=100;
+	float ascend;
+	bool arrived=false;
+	public GameObject container;
+	public GameObject groundplate;
+	public GameObject ropeA;
+	public GameObject ropeB;
+	float cooldown=0.5f;
+	// Use this for initialization
+	void Start () {
+		 i = distance;
+		ascend = height;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//print (distance / Time.deltaTime * speed);
+
+		if (i > 0) {
+			float travelled = speed / Time.deltaTime * timemultiplier;
+			this.transform.position += new Vector3 (0, 0, travelled);
+			i -= travelled;
+			//print (i);
+		}
+
+		if (i <= 0.0f) {
+
+			if(container)
+			{//groundplate.transform.position = Vector3.Lerp(groundplate.transform.position, new Vector3(0.0f,1.0f, 0.0f), Time.deltaTime);
+				if(height>0)
+				{
+					container.transform.position += new Vector3(0,-0.05f,0);
+					groundplate.transform.position += new Vector3(0,-0.05f,0);
+					ropeA.transform.position += new Vector3(0,-0.025f,0);
+					ropeA.transform.localScale += new Vector3(0,0.025f,0);
+					ropeB.transform.position += new Vector3(0,-0.025f,0);
+					ropeB.transform.localScale += new Vector3(0,0.025f,0);
+					height--;}
+
+			}
+			if (height<=0){
+
+				
+				cooldown -= Time.deltaTime;
+
+				//print("cooldown: "+cooldown);
+				
+				if(cooldown < 0.0f)
+				{
+					arrived=true;
+					
+				}
+
+						
+					}
+
+			if (arrived==true){
+
+				print("ascend");
+				if(ascend>0)
+				{
+
+					container.transform.position += new Vector3(0,0.05f,0);
+					ropeA.transform.position += new Vector3(0,0.025f,0);
+					ropeA.transform.localScale += new Vector3(0,-0.025f,0);
+					ropeB.transform.position += new Vector3(0,0.025f,0);
+					ropeB.transform.localScale += new Vector3(0,-0.025f,0);
+					ascend--;}
+			}
+
+
+		}
+	}
+
+}
