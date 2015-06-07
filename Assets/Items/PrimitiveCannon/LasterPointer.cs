cb00901bae5046ae946f71fc16bfcda5ca4437ac
Kezzo <jon_jelinski@t-online.de>
2015-06-07 03:23:11 +0200

    - Cleaned up the project structure - Fixed a bug where the laser pointer failed to be rendered, because the level got no boundaries

--- Assets/Items/PrimitiveCannon/LasterPointer.cs
+++ Assets/Items/PrimitiveCannon/LasterPointer.cs
@@ -8,18 +8,22 @@ public class LasterPointer : MonoBehaviour {
 
 	RaycastHit rayCastHit;
 	Ray ray;
+
+	Vector3 endPoint;
 	
 	// Update is called once per frame
 	void Update () 
 	{
 		lineRenderer.SetPosition(0, originTransform.position);
-
-		if(Physics.Raycast(originTransform.position, this.transform.forward, out rayCastHit))
-		{
-			lineRenderer.SetPosition(1, rayCastHit.point);
+		
+		if (Physics.Raycast (originTransform.position, this.transform.forward, out rayCastHit, 50.0f)) {
+			endPoint = rayCastHit.point;
 			//Debug.DrawLine(originTransform.position, rayCastHit.point);
+		} else {
+			endPoint = originTransform.position + this.transform.forward * 50.0f;
 		}
 
+		lineRenderer.SetPosition (1, endPoint);
 
 	}
 }
