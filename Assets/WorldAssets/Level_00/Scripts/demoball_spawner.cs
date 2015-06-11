ae7477f7dfd81eee577a4289175958824916b0e2
Darcody <d-jordan@gmx.net>
2015-06-11 14:17:31 +0200

    Added "throwing" to throwing demo.

---
+++ Assets/WorldAssets/Level_00/Scripts/demoball_spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class demoball_spawner : MonoBehaviour {
+	public GameObject balls;
+	public float spawnspeed=2.0f;
+	float counter =0;
+	// Use this for initialization
+	void Start () {
+		counter=spawnspeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter-=Time.deltaTime;
+		if (counter < 0) {
+
+			GameObject clone;
+			clone=Instantiate(balls, transform.position, transform.rotation) as GameObject;
+
+			//Destroy(clone, 5);
+			counter=spawnspeed;
+		}
+	}
+}
