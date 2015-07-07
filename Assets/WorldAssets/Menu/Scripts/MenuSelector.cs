2594ff368950cc6f899230649cca202d83fb46fc
Fabian <fabianmueller5@gmx.net>
2015-07-07 23:54:44 +0200

    -Added a Menu

---
+++ Assets/WorldAssets/Menu/Scripts/MenuSelector.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuSelector : MonoBehaviour {
+
+	RaycastHit hit;
+	GameObject lastHitItem;
+	SixenseInput.Controller hydraLeft;
+	SixenseInput.Controller hydraRight;
+
+	string buttonHighlited;
+
+	void Update()
+	{
+		hydraRight = SixenseInput.Controllers[1];
+		hydraLeft = SixenseInput.Controllers[0];
+
+		if(hydraLeft.GetButtonDown(SixenseButtons.TRIGGER) || hydraRight.GetButtonDown(SixenseButtons.TRIGGER))
+		{
+			if(buttonHighlited == "StartTut")
+			{
+				Application.LoadLevelAsync("Level_00");
+			}
+			else if(buttonHighlited == "StartHub")
+			{
+				Application.LoadLevelAsync("hub");
+			}
+			else if(buttonHighlited == "Exit")
+			{
+				Application.Quit();
+			}
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		if(Physics.Raycast(this.transform.position, this.transform.TransformDirection(Vector3.forward),out hit,50))
+		{
+			GameObject hitItem = hit.collider.gameObject;
+			if(hitItem.layer == 5)
+			{
+				hitItem.GetComponent<MeshRenderer>().material.color = Color.red;
+				buttonHighlited = hitItem.transform.parent.name;
+			}
+			if(hitItem != lastHitItem)
+			{
+				if(lastHitItem != null && lastHitItem.layer == 5)
+				{
+					lastHitItem.GetComponent<MeshRenderer>().material.color = Color.white;
+					buttonHighlited = "";
+				}
+			}
+			lastHitItem = hitItem;
+		}
+	}
+}
