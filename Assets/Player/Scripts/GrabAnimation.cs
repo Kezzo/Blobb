e330ce3ced5a3c4b445f065ef3d4fc7b45537fd8
Kezzo <jon_jelinski@t-online.de>
2015-07-09 21:02:57 +0200

    - Increased hand-sensitivity in game by 0.001 on the Z- and Y-Axis - Changed grab-animation to scale the mesh rather then rotate it, to prevent mesh problems - Removed one of the targets at the throwing-range and increased the other - Added wall at the throwing-range to prevent balls from rolling to the left - Increased 'ball-reset-area' at the throwing-range to reset the balls more reliable - Decreased height of the block the were cannons at the shooting-range lie on - Increased opening-trigger on the second and third door in the tutorial-level, to prevent them from closing to early

--- Assets/Player/Scripts/GrabAnimation.cs
+++ Assets/Player/Scripts/GrabAnimation.cs
@@ -4,19 +4,23 @@ using System.Collections;
 public class GrabAnimation : MonoBehaviour {
 
 	public bool isRight;
-	Quaternion rotation;
+	//Quaternion rotation;
+
 	void LateUpdate () 
 	{
+
 		if(isRight)
 		{
-			rotation = Quaternion.Euler(new Vector3(-90.0f + SixenseInput.Controllers[1].Trigger * 90.0f,0,0));
+			transform.localScale = new Vector3(transform.localScale.x, 1 - (SixenseInput.Controllers[1].Trigger / 2), 1 - (SixenseInput.Controllers[1].Trigger / 2));
+			//rotation = Quaternion.Euler(new Vector3(-90.0f + SixenseInput.Controllers[1].Trigger * 90.0f,0,0));
 		}
 		else
 		{
-			rotation = Quaternion.Euler(new Vector3(90.0f + SixenseInput.Controllers[0].Trigger * 90.0f,0,0));
+			transform.localScale = new Vector3(transform.localScale.x, 1 - (SixenseInput.Controllers[0].Trigger / 2), 1 - (SixenseInput.Controllers[0].Trigger / 2));
+			//rotation = Quaternion.Euler(new Vector3(90.0f + SixenseInput.Controllers[0].Trigger * 90.0f,0,0));
 		}
 
-		this.transform.localRotation = rotation;
+		//this.transform.localRotation = rotation;
 	
 	}
 }
